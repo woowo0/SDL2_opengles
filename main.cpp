310018c564011232d4ed287483a5e23bb1d7b1dd
woowo0 <woowo0@mail.ru>
2019-04-18 22:09:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ int main( void )
 	//glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "vert.glsl", "frag.glsl" );
 
 	static const GLfloat g_vertex_buffer_data[] = { 
 		-0.9f, -0.9f, 0.0f,
